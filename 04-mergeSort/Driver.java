e1a949877b7d6a24d1808cc9b373630266c67a70
Yicheng Wang <alex.wyc2098@protonmail.ch>
2015-02-25 08:42:19 -0500

    updated

---
+++ 04-mergeSort/Driver.java
@@ -0,0 +1,10 @@
+import java.util.*;
+import java.io.*;
+import mergeSort;
+
+public class Driver {
+
+	public static void main(String[] args) {
+
+	}
+}
---
+++ 04-mergeSort/MergeSort.java
@@ -0,0 +1,44 @@
+import java.util.*;
+import java.io.*;
+
+public class mergeSort {
+	
+	public static ArrayList<Integer> mergeSort(ArrayList<Integer> data) {
+		if (data.size() == 1) {
+			return data;
+		}
+
+		else {
+			ArrayList<Integer> A, B = new ArrayList<Integer>();
+
+			for (int i = 0 ; i < data.size() / 2 ; i++) {
+				A.add(data.get(i));
+			}
+			for (int i = data.size() / 2 ; i < data.size() ; i++) {
+				B.add(data.get(i));
+			}
+
+			mergeSort(A);
+			mergeSort(B);
+			return merge(A, B);
+		}
+	}
+
+	private ArrayList<Integer> merge(ArrayList<Integer> A, ArrayList<Integer> B) {
+		ArrayList<Integer> result = new ArrayList<Integer>();
+
+		while(A.size() != 0 && B.size() != 0) {
+			if (A.get(0) > B.get(0)) {
+				result.add(B.remove(0));
+			}
+			else {
+				result.add(A.remove(0));
+			}
+		}
+
+		result.addAll(A);
+		result.addAll(B);
+		
+		return result;
+	}
+}
