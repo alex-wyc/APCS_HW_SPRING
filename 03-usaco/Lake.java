3f3e9e22e856313fea42c5a91ba357d26f440b60
Yicheng Wang <alex.wyc2098@protonmail.ch>
2015-02-25 21:24:56 -0500

    updated

--- 03-usaco/Lake.java
+++ 03-usaco/Lake.java
@@ -1,4 +1,4 @@
-import java.uitl.*;
+import java.util.*;
 import java.io.*;
 
 public class Lake {
@@ -11,16 +11,16 @@ public class Lake {
 	private int R, C, E, N;
 
 	// constructor, reads in file
-	public lake(String path) {
+	public Lake(String path) {
 		BufferedReader br = null;
 		String line = null;
 		int lineNum = 0;
 		String[] parts = null;
 		try {
 			br = new BufferedReader(new FileReader(path));
-			line = br.nextLine();
-			while (line != null) {
+			while ((line = br.readLine()) != null) {
 				parts = line.split(" ");
+
 				if (lineNum == 0) {
 					R = Integer.parseInt(parts[0]);
 					C = Integer.parseInt(parts[1]);
@@ -29,35 +29,52 @@ public class Lake {
 					elevations = new int[R][C];
 					instructions = new int[N][3];
 				}
-				if (lineNum <= R) {
+
+				else if (lineNum <= R) {
 					for (int i = 0 ; i < C ; i++) {
-						elevations[lineNum - 1][i] = parts[i];
+						elevations[lineNum - 1][i] = Integer.parseInt(parts[i]);
 					}
 				}
 
 				else {
 					for (int i = 0 ; i < 3 ; i++) {
-						instructions[lineNum - R - 1][i] = parts[i];
+						instructions[lineNum - R - 1][i] = Integer.parseInt(parts[i]);
 					}
 				}
+
 				lineNum++;
 			}
+		} catch (Exception e) {
+			System.out.println("Oops, something went wrong!");
+			e.printStackTrace();
 		}
+	}
+
+	public String toString() {
+		String s = "";
+		for (int i = 0 ; i < R ; i++) {
+			for (int j = 0 ; j < C ; j++) {
+				s += elevations[i][j] + " ";
+			}
+			s += "\n";
+		}
+
+		return s;
 	}
 
 	public void performInstructions() {
 		for (int[] i : instructions) {
 			int currentMax = 0;
-			for (int y = i[0] ; y < i[0] + 3 ; y++) {
-				for (int x = i[1] ; x < i[1] + 3 ; x++) {
+			for (int y = i[0] - 1 ; y < i[0] + 2 ; y++) {
+				for (int x = i[1] - 1 ; x < i[1] + 2 ; x++) {
 					if (elevations[y][x] > currentMax) {
 						currentMax = elevations[y][x];
-`					}
+					}
 				}
 			}
 			currentMax -= i[2];
-			for (int y = i[0] ; y < i[0] + 3 ; y++) {
-				for (int x = i[1] ; x < i[1] + 3 ; x++) {
+			for (int y = i[0] - 1 ; y < i[0] + 2 ; y++) {
+				for (int x = i[1] - 1; x < i[1] + 2 ; x++) {
 					if (elevations[y][x] > currentMax) {
 						elevations[y][x] = currentMax;
 					}
@@ -70,7 +87,7 @@ public class Lake {
 		int sum = 0;
 		for (int y = 0 ; y < R ; y++) {
 			for (int x = 0 ; x < C ; x++) {
-				elevations[y][x] -= E;
+				elevations[y][x] = E - elevations[y][x];
 				if (elevations[y][x] < 0) {
 					elevations[y][x] = 0;
 				}
@@ -81,7 +98,7 @@ public class Lake {
 		return sum * 12 * 12 * 6 * 6;
 	}
 
-	public static void Main(String[] args) {
+	public static void main(String[] args) {
 		Lake lk = new Lake(args[0]);
 		lk.performInstructions();
 		int ans = lk.calculateLake();
