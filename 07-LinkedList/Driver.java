6cdac7dc338c7b2e058662d91e4519148933bdde
Yicheng Wang <alex.wyc2098@protonmail.ch>
2015-03-12 08:39:24 -0400

    updated

---
+++ 07-LinkedList/Driver.java
@@ -0,0 +1,9 @@
+public class Driver {
+
+    public static void main(String[] args) {
+        Llist ll = new Llist();
+        ll.add("hello");
+        ll.add("world");
+        System.out.println(ll);
+    }
+}
