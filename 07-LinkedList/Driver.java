45008627b16e0204f3d383b6a3e7c93720a55a49
Yicheng Wang <alex.wyc2098@protonmail.ch>
2015-03-14 17:03:22 -0400

    solution added

--- 07-LinkedList/Driver.java
+++ 07-LinkedList/Driver.java
@@ -1,9 +1,23 @@
+import java.util.Random;
+
 public class Driver {
 
     public static void main(String[] args) {
-        Llist ll = new Llist();
-        ll.add("hello");
-        ll.add("world");
-        System.out.println(ll);
+        Random randgen = new Random();
+        Llist l = new Llist();
+        for (int i = 0 ; i < 10 ; i++) {
+            l.add("" + i);
+        }
+
+        System.out.println(l);
+
+        System.out.println(l.find(5) + "\n" + l.find(0) + "\n" + l.find(12));
+
+        l.insert(3, "hello");
+        System.out.println("done");
+        l.insert(0, "hi");
+        System.out.println("done");
+        System.out.println(l);
+        l.insert(15, "bhsjdl");
     }
 }
--- 07-LinkedList/Llist.java
+++ 07-LinkedList/Llist.java
@@ -11,9 +11,41 @@ public class Llist {
         String s = "";
         Node tmp;
         for (tmp = l ; tmp != null ; tmp = tmp.getNext()) {
-            s = s + tmp + " -> ";
+            s = s + tmp + " --> ";
         }
         s = s + "null";
         return s;
+    }
+
+    public String find(int n) {
+        Node tmp = l;
+        for (int i = 0 ; i < n && tmp != null ; i++) {
+            tmp = tmp.getNext();
+        }
+
+        if (tmp == null)
+            return "null";
+        return tmp.getData();
+    }
+
+    public void insert(int n, String s) {
+        Node newEl = new Node(s);
+        Node currentNode = l;
+        if (n == 0) {
+            add(s);
+            return;
+        }
+
+        for (int i = 0 ; i < n - 1 && currentNode != null && currentNode.getNext() != null; i++) {
+            currentNode = currentNode.getNext();
+        }
+
+        if (currentNode == null || currentNode.getNext() == null) {
+            throw new IndexOutOfBoundsException();
+        }
+
+        newEl.setNext(currentNode.getNext());
+        currentNode.setNext(newEl);
+        //currentNode = newEl;
     }
 }
