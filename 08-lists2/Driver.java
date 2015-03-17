185bd5be59dafe2b5b0847e45a141a9cfc057ad7
Yicheng Wang <alex.wyc2098@protonmail.ch>
2015-03-17 18:54:41 -0400

    updated

---
+++ 08-lists2/Driver.java
@@ -0,0 +1,30 @@
+import java.util.Random;
+
+public class Driver {
+
+    public static void main(String[] args) {
+        Random randgen = new Random();
+        Llist l = new Llist();
+        for (int i = 0 ; i < 10 ; i++) {
+            l.add("" + randgen.nextInt(100));
+        }
+
+        System.out.println(l);
+
+        System.out.println(l.get(5) + "\n" + l.get(0) + "\n" + l.get(12));
+
+        l.add(3, "hello");
+        System.out.println("done");
+        l.add(0, "hi");
+        System.out.println("done");
+        System.out.println(l);
+        System.out.println(l.length());
+        l.add(12, "end");
+        System.out.println(l);
+        System.out.println(l.get(12));
+//        l.add(15, "bhsjdl");
+
+        System.out.println(l.remove(10));
+        System.out.println(l);
+    }
+}
