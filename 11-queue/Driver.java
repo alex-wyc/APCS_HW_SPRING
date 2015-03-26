12ee12d0add6caccd9a1fcceaebb4e1839b1fbd4
Yicheng Wang <alex.wyc2098@protonmail.ch>
2015-03-26 08:39:36 -0400

    updated

--- 11-queue/Driver.java
+++ 11-queue/Driver.java
@@ -2,6 +2,7 @@ import java.lang.Integer;
 
 public class Driver {
     public static void main(String[] args) {
+        /*
         myQueue<Integer> q1 = new myQueue<Integer>();
         System.out.println(q1.empty());
         for (int i = 0 ; i < 10 ; i++) {
@@ -13,5 +14,18 @@ public class Driver {
         System.out.println(q1.head());
         System.out.println(q1.dequeue());
         System.out.println(q1);
+        */
+
+        ArrayQueue2<Integer> q2 = new ArrayQueue2<Integer>(10);
+        System.out.println(q2.empty());
+        for (int i = 0 ; i < 10 ; i++) {
+            q2.enqueue(i);
+        }
+        System.out.println(q2.empty());
+
+        System.out.println(q2);
+        System.out.println(q2.head());
+        System.out.println(q2.dequeue());
+        System.out.println(q2);
     }
 }
